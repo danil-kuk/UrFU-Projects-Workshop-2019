49e157f0663b44c1502876a641331249aa6231c5
danil-kuk <danilkuk5@gmail.com>
2019-12-12 17:05:35 +0500

    Base

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./styles/App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
