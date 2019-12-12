7a28a18716cf38c98fade4b7c34a0a15f98d8c74
danil-kuk <danilkuk5@gmail.com>
2019-12-12 17:43:26 +0500

    Добавлено описание

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import "./styles/App.css";
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
