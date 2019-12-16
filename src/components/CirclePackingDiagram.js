5bf4d8aa6a2cbbeb7986d012cd208c6d92ab61aa
danil-kuk <danilkuk5@gmail.com>
2019-12-16 18:28:27 +0500

    Добавлена диаграмма Направлений

---
+++ src/components/CirclePackingDiagram.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ResponsiveBubble } from '@nivo/circle-packing'
+import "../styles/CirclePackingDiagram.css"
+
+const CirclePackingDiagram = (props) => (
+    <div className="circle-parcking-holder">
+        <ResponsiveBubble
+            root={props.data}
+            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+            identity="name"
+            value="value"
+            colors={{ scheme: 'blues' }}
+            padding={6}
+            label="value"
+            labelTextColor={{ from: 'color', modifiers: [['darker', 0.8]] }}
+            borderWidth={2}
+            borderColor={{ from: 'color' }}
+            fill={[{ match: { depth: 1 }, id: 'lines' }]}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={12}
+        />
+    </div>
+)
+
+export default CirclePackingDiagram
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,12 @@
 import React from "react";
+import CirclePackingDiagram from "../components/CirclePackingDiagram"
+import data from "../../assets/data.json"
 
 const Courses = () => {
   return (
     <div className="container">
       <h1>Направления</h1>
+      <CirclePackingDiagram data={data}/>
     </div>
   );
 };
