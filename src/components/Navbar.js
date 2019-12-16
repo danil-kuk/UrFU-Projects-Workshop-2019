6bf785c0b3de537eedfd26a55cf359855730f5bc
danil-kuk <danilkuk5@gmail.com>
2019-12-16 09:37:12 +0500

    Добавлен логотип на панель навигации

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import logo from '../../assets/logo.svg';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <ul id="navbar">
-          <li><Link to="/">Главная</Link></li>
-          <li><Link to="/customers">Заказчики</Link></li>
-          <li><Link to="/students">Студенты</Link></li>
-          <li><Link to="/courses">Направления</Link></li>
-        </ul>
+        <div className="logo-holder">
+          <img src={logo} alt="Logo" id="logo" />
+        </div>
+        <div className="nav-holder">
+          <ul id="navbar">
+            <li><Link to="/">Главная</Link></li>
+            <li><Link to="/customers">Заказчики</Link></li>
+            <li><Link to="/students">Студенты</Link></li>
+            <li><Link to="/courses">Направления</Link></li>
+          </ul>
+        </div>
       </div>
     );
   }
