d994bbc439cd389ce63312bb180484c267f74b71
danil-kuk <danilkuk5@gmail.com>
2019-12-21 11:11:52 +0500

    Создана карточка заказчика

---
+++ src/components/CustomerCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/CustomerCard.css"
+import CoursesIcons from "./CoursesIcons"
+
+const CustomerCard = (props) => {
+    return (
+        <div className="customer-card">
+            <div className="card-title">
+                <div className="img-holder">
+                    <img src={props.img_link}></img>
+                </div>
+                <h4>{props.title}</h4>
+            </div>
+            <div className="customer-info">
+                <p>
+                    <b>Тип организации: </b>{props.org_type} <br />
+                    <b>Область деятельности: </b>{props.activity_type} <br />
+                    <b>Размер организации: </b>{props.org_size} <br />
+                    <b>Расположение: </b>{props.org_place} <br />
+                    <b>Направления: </b>
+                </p>
+            </div>
+            <div className="courses">
+                {props.courses.map(c => (
+                    <div className="course-icon">
+                        <CoursesIcons
+                            icon={c}
+                            key={props.org_type + c}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CustomerCard;
--- src/pages/CustomersInfo.js
+++ src/pages/CustomersInfo.js
@@ -1,9 +1,21 @@
 import React from "react";
+import CustomerCard from "../components/CustomerCard"
 
 const CustomersInfo = () => {
   return (
     <div className="container">
       <h1>Пор заказчиков</h1>
+      <CustomerCard
+        title="Уральский Федеральный Университет"
+        description="Это текст про заказчика, он скорее всего будет достаточно длинным, ну и стоит вставить ещё немного слов"
+        org_type="Университет"
+        activity_type="Учебная, научная"
+        org_size="Крупная"
+        img_link="https://urfu.ru/fileadmin/user_upload/common_files/about/brand/UrFULogo_U.png"
+        org_place="Екатеринбург"
+        courses={["machine-learning", "games", "bots", "robots", "digital-marketing",
+          "vr", "web-development", "mobile", "information-systems", "math-modelling"]}
+      />
     </div>
   );
 };
