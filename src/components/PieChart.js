fe424781c2a4f84683c9435f9ee969357dd4e08a
danil-kuk <danilkuk5@gmail.com>
2019-12-30 14:14:47 +0500

    Добавлен половой состав студентов

---
+++ src/components/PieChart.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { ResponsivePie } from '@nivo/pie'
+
+const Chart = (props) => {
+  return (
+    <div className="pie-chart-holder">
+      <ResponsivePie
+        data={props.data}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        startAngle={props.startAngle}
+        endAngle={props.endAngle}
+        innerRadius={0.45}
+        padAngle={2}
+        colors={props.colorScheme}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={4}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={13}
+        radialLabelsLinkHorizontalLength={10}
+        radialLabelsLinkStrokeWidth={3}
+        radialLabelsLinkColor={{ from: 'color' }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        theme={{
+            labels: {
+                text: {
+                    fontSize: 20,
+                    fontFamily: ""
+                }
+            }
+        }}
+    />
+    </div>
+  );
+};
+
+export default Chart;
--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,9 +1,13 @@
 import React from "react";
+import data from '../../assets/data/Students-data.json'
+import PieChart from '../components/PieChart'
 
 const Customers = () => {
   return (
     <div className="container">
       <h1>Студенты</h1>
+      <h2>Половой состав</h2>
+      <PieChart data={data.gender} colorScheme={{ scheme: 'pastel1' }} />
     </div>
   );
 };
