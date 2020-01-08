77c56a7d19e99d4267a27dd71e4935abdca01e55
danil-kuk <danilkuk5@gmail.com>
2020-01-08 14:11:30 +0500

    Заполнена Хронология

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,24 +4,27 @@ import "../styles/Diagrams.css"
 
 const MyTimeline = (props) => (
   <div>
-    <Timeline lineColor={'#ddd'} className="pw-timeline">
-      {props.data.map(element => (
-        <TimelineItem
-          key={element.date + element.title}
-          dateText={element.date}
-          style={{ color: '#6BAED6' }}
-          dateStyle={{
-            left: 12
-          }}
-          dateInnerStyle={{
-            background: '#6BAED6',
-            fontSize: 18
-          }}
-        >
-          <h3>{element.title}</h3>
-          <p>{element.content}</p>
-        </TimelineItem>
-      ))}
+    <Timeline lineColor={'#C8CED7'} className="pw-timeline">
+      {props.data.map(element => {
+        let elemColor = element.color != null ? element.color : '#6BAED6'
+        return (
+          <TimelineItem
+            key={element.date + element.title}
+            dateText={element.date}
+            style={{ color: elemColor }}
+            dateStyle={{
+              left: 12
+            }}
+            dateInnerStyle={{
+              background: elemColor,
+              fontSize: 18
+            }}
+          >
+            <h3>{element.title}</h3>
+            <p>{element.content}</p>
+          </TimelineItem>
+        )
+      })}
     </Timeline>
   </div>
 )
