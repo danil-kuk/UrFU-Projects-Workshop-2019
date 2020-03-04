d262c561c0db933094e51746f3e09665929aeb00
danil-kuk <danilkuk5@gmail.com>
2020-03-04 11:05:48 +0500

    Добавлена информация о командах

--- src/pages/TeamsResults.js
+++ src/pages/TeamsResults.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import RadarDiagram from '../components/RadarDiagram'
 import radarData from '../../assets/data/TeamsResults-data.json'
+import teamsInfo from '../../assets/data/TeamsResultsInfo.json'
 import DataPreparation from '../components/RadarDataPreparation'
 import '../styles/Presentation.css'
 
-var allKeys = Object.keys(radarData)
+var allKeys = []
+allKeys.push(Object.keys(radarData)[0])
+allKeys = allKeys.concat(Object.keys(radarData).slice(1).sort())
 var newData = DataPreparation(radarData)
 
 class Presentation extends React.Component {
@@ -48,6 +51,10 @@ class Presentation extends React.Component {
                 levels={3}
               />
             </div>
+            {this.state.value[0] != allKeys[0] &&
+              team_info(this.state.value[0])}
+            {this.state.value[0] == allKeys[0] &&
+              general_info(this.state.value[0])}
           </div>
         </div>
       </div>
@@ -56,3 +63,43 @@ class Presentation extends React.Component {
 }
 
 export default Presentation
+
+function team_info(selectedTeam) {
+  const item = teamsInfo[selectedTeam]
+  return (
+    <div className='team-results-info'>
+      <h3>Информация о команде</h3>
+      <p>
+        Средний балл команды: {item.score}
+        <br />
+        Проект: {item.project}
+        <br />
+        Трек: {item.track}
+        <br />
+        Куратор: {item.curator}
+        <br />
+        Количество участников: {item.members_count}
+      </p>
+    </div>
+  )
+}
+
+function general_info(key) {
+  const item = teamsInfo[key]
+  return (
+    <div className='team-results-info'>
+      <h3>Информация</h3>
+      <p>
+        Средний итоговый балл: {item.score}
+        <br />
+        Всего проектов: {item.projects_count}
+        <br />
+        Количество направлений: {item.courses_count}
+        <br />
+        Количество кураторов: {item.curators_count}
+        <br />
+        Количество команд: {item.teams_count}
+      </p>
+    </div>
+  )
+}
