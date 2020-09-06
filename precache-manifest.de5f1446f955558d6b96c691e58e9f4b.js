5ee16acf4a8d27b9f1ae43eed75fc7ff066519be
danil-kuk <danilkuk5@gmail.com>
2020-09-06 17:28:46 +0500

    Deploy 06-09-2020 17:28

---
+++ precache-manifest.de5f1446f955558d6b96c691e58e9f4b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069d2e49358600f0cf57dda12db26b7a",
+    "url": "/UrFU-Projects-Workshop-2019/index.html"
+  },
+  {
+    "revision": "a94ddb62895afa809a3a",
+    "url": "/UrFU-Projects-Workshop-2019/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "c1354bbe81867baa9093",
+    "url": "/UrFU-Projects-Workshop-2019/static/css/main.3ba3c45d.chunk.css"
+  },
+  {
+    "revision": "a94ddb62895afa809a3a",
+    "url": "/UrFU-Projects-Workshop-2019/static/js/2.b0aebec5.chunk.js"
+  },
+  {
+    "revision": "dc1a0f74e6e6646bbe26a3fd06ccaa35",
+    "url": "/UrFU-Projects-Workshop-2019/static/js/2.b0aebec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1354bbe81867baa9093",
+    "url": "/UrFU-Projects-Workshop-2019/static/js/main.04676a1b.chunk.js"
+  },
+  {
+    "revision": "2992b6994e0a1f3ad3ca",
+    "url": "/UrFU-Projects-Workshop-2019/static/js/runtime-main.9cf6f78b.js"
+  },
+  {
+    "revision": "ef500ecf858f87e272f16dacf501a46a",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/3d-modeling.ef500ecf.svg"
+  },
+  {
+    "revision": "551f5d12731d3747e5182bd3e8a7683c",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/Bahnschrift.551f5d12.ttf"
+  },
+  {
+    "revision": "8db2b282875f496453c71a257b79ab1b",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/analyst.8db2b282.svg"
+  },
+  {
+    "revision": "0b9efa46c8ed4687c442f11af66b2303",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/artist.0b9efa46.svg"
+  },
+  {
+    "revision": "e4ca3f04668597d3477b7aa64b05f44f",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/broadcast.e4ca3f04.svg"
+  },
+  {
+    "revision": "303b9a7cd48407205d27999a275ec63b",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/chat-bubbles-with-ellipsis.303b9a7c.svg"
+  },
+  {
+    "revision": "2dbbdde58d870fb4190f9eab5e7f7c9a",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/chip.2dbbdde5.svg"
+  },
+  {
+    "revision": "d11a3aac7ea4384ea9e5b187352496cf",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/designer.d11a3aac.svg"
+  },
+  {
+    "revision": "6e12f4b5cbb2020fce7ab2a9785ace46",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/developer.6e12f4b5.svg"
+  },
+  {
+    "revision": "0be8847148797ed6d4e83dc4c629383a",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/explorer.0be88471.svg"
+  },
+  {
+    "revision": "0a493a7a3ab4d1253cbd88bef7db7d2f",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/gamepad-controller.0a493a7a.svg"
+  },
+  {
+    "revision": "5c1a3229c2e0098a7f0e1a31a6241027",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/internet.5c1a3229.svg"
+  },
+  {
+    "revision": "e491f58465b7b3799e422e10c0ec6f08",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/logo.e491f584.svg"
+  },
+  {
+    "revision": "5e8660efc23e160816e27ffddd8c5cb4",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/machine-learning.5e8660ef.svg"
+  },
+  {
+    "revision": "6aa4f844ec1542fd8aef9b0d199fdd1c",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/manager.6aa4f844.svg"
+  },
+  {
+    "revision": "763768537638657483e1e482b85943bc",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/mechanical-arm.76376853.svg"
+  },
+  {
+    "revision": "431523dbef3808c89e9ac8f9aea3318a",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/placeholder.431523db.svg"
+  },
+  {
+    "revision": "431d53aa317206958c24ce9d7290176c",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/smartphone.431d53aa.svg"
+  },
+  {
+    "revision": "93cb39e329fd6adba351edbb8ee96226",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/team.93cb39e3.svg"
+  },
+  {
+    "revision": "fe61cde46ef2bef8923a9d3a7c05431f",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/tester.fe61cde4.svg"
+  },
+  {
+    "revision": "f1e8dd5a8b6bf8c663b96659df412d9a",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/virtual-reality.f1e8dd5a.svg"
+  },
+  {
+    "revision": "d6714a1cd03f013bc3dc318d275d0fa8",
+    "url": "/UrFU-Projects-Workshop-2019/static/media/Итоговая визуализация.d6714a1c.svg"
+  }
+]);
